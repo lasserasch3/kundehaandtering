34bd9884a1db41d2682b4acea2f5fa53ad684c7f
Lasse Rasch <info@lasserasch.dk>
2023-10-25 12:58:58 +0200

    tilføjet knap

--- CustomerEditor/CustomerEditorGUI/Form1.cs
+++ CustomerEditor/CustomerEditorGUI/Form1.cs
@@ -20,7 +20,7 @@ namespace CustomerEditorGUI
         private void Form1_Load(object sender, EventArgs e)
         {
             LoadCustomerList();
-           
+
             dataGridView1.DesignUCLDataGridView();
         }
 
